761cd066e81f40aee459bae99b7387d2ceb8ef25
zulfafani <zulfafani@gmail.com>
2023-05-10 12:48:23 +0700

    test

--- FirstTutorial/Program.cs
+++ FirstTutorial/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Mynamespace
+{
+class Cat {
+  //field
+  string breed;
+  //method
+  public void eat() {
+  }
+}
+}
