454ab9f6d4258c70a22a8775b45ae37ea6dd42b0
zulfafani <zulfafani@gmail.com>
2023-05-22 16:42:29 +0700

    init commit

---
+++ Day4/DataType/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace DataType
+{
+	class Cat
+	{
+		public Leg catLeg;
+		
+		public void SetLeg()
+		{
+			Console.WriteLine("setleg");
+		}
+	}
+	
+	class Leg
+	{
+		public int Leg;
+		
+		public Leg(int x)
+		{
+			catLeg = x;
+		}
+		
+	}
+	
+	class Program
+	{
+		static void main()
+		{
+			Cat leg = new Cat();
+			leg.SetLeg();
+		}
+		
+	}
+
+	
+}
---
+++ Day4/Tuple/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+static class Program
+{
+	static void Main()
+	{
+		Cycle cycle = new Cycle("Polygon");
+		Car car = new Car("Subaru");
+		Cargo myCargo = new Cargo(car, cycle);
+
+		int maxWeight = myCargo.checkWeight();
+		Car myCar = myCargo.checkCar();
+		Cycle myCycle = myCargo.checkCycle();
+
+		Console.WriteLine(myCar.GetBrand());
+		Console.WriteLine(myCycle.GetBrand());
+	}
+}
+class Cargo 
+{
+	private Car _cargoCar;
+	private Cycle _cargoCycle;
+	private int _totalWeight;
+	public Cargo(Car cargoCar, Cycle cargoCycle)
+	{
+		_cargoCar = cargoCar;
+		_cargoCycle = cargoCycle;
+		_totalWeight = 1000;
+	}
+	public int checkWeight() 
+	{
+		return _totalWeight;
+	}
+	public Car checkCar() 
+	{
+		return _cargoCar;
+	}
+	public Cycle checkCycle() 
+	{
+		return _cargoCycle;
+	}
+}
+class Car
+{
+	private string _carName;
+	public Car(string brand) 
+	{
+		_carName = brand;
+	}
+	public string GetBrand() 
+	{
+		return _carName;
+	}
+}
+class Cycle
+{
+	private string _cycleName;
+	public Cycle(string brand) 
+	{
+		_cycleName = brand;
+	}
+	public string GetBrand()
+	{
+		return _cycleName;
+	}
+}
