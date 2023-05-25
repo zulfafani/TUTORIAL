b1c40840d18118513adc791f2b02507aff23bc25
zulfafani <zulfafani@gmail.com>
2023-05-25 15:40:01 +0700

    initial commit

---
+++ Day9/Enum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+	enum Level
+	{
+		Low,
+		Medium,
+		High
+	}
+	public static void Main(string[] args)
+	{
+		Level status = Level.Low;
+
+		switch(status) 
+  		{
+    		case Level.Low:
+      		Console.WriteLine("Low level");
+      		break;
+    		case Level.Medium:
+       		Console.WriteLine("Medium level");
+      		break;
+    		case Level.High:
+      		Console.WriteLine("High level");
+      		break;
+  }
+	}
+}
---
+++ Day9/TryCatch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		try
+		{
+			Console.WriteLine("User input: ");
+			string userInput = Console.ReadLine();
+			int number = int.Parse(userInput); //Unhandled exception if user input string
+			Console.WriteLine(number);
+			
+			//if(int.TryParse(userInput))
+		}
+		catch (Exception e)
+		{
+				Console.WriteLine(e.Message); //"The input string was not in a correct format"
+		}
+	}
+}
