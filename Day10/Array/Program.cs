1e8aee4978bd9592f51378858f22fd05ae801541
zulfafani <zulfafani@gmail.com>
2023-05-29 16:57:34 +0700

    initial commit

---
+++ Day10/Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		string[] days = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+		int[] numbers = {1,2,3,4,5};
+		
+		for(int i = 0; i < days.Length; i++)
+		{
+			Console.WriteLine(days[i]);
+		}
+		
+		foreach(int j in numbers)
+		{
+			Console.WriteLine(j);
+		}
+		
+		bool holidays = days.Contains("Sunday");
+		if(holidays == true)
+		{
+			Console.WriteLine("Playing everyday");
+		}
+		else
+		{
+			Console.WriteLine("Not the days");
+		}
+	}
+}
---
+++ Day10/List/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListDays
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			List<string> days = new List<string>();
+			days.Add("Sunday");
+			days.Add("SUNDAY");
+			days.Add("MONDAY");
+			days.Add("Sund4y");
+			days.Add("MONDAY");
+
+			foreach (string i in days)
+			{
+				if (days.Contains(i))
+				{
+					continue; //not contain dupplicate maka next line dijalankan
+				}
+
+				bool isUpperCase = true; //value default dulu
+				foreach (char character in i)
+				{
+					if (!char.IsUpper(character))
+					{
+						isUpperCase = false;
+					}
+				}
+				if (isUpperCase)
+				{
+					days.Add(i);
+				}
+			}
+			Console.WriteLine(string.Join(days));
+
+		}
+	}
+}
